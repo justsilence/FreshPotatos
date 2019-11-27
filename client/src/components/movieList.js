120262a19d53ad371b273cddc9c5cfa24e485cb0
MarkKai <742714625@qq.com>
2019-11-27 15:30:44 -0500

    first version of movie list

---
+++ client/src/components/movieList.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import UpdateIcon from '@material-ui/icons/Update';
+import DeleteIcon from '@material-ui/icons/Delete';
+import MovieIcon from '@material-ui/icons/Movie';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    maxWidth: 752,
+  },
+  demo: {
+    backgroundColor: theme.palette.background.paper,
+  },
+  title: {
+    margin: theme.spacing(4, 0, 2),
+  },
+}));
+
+export default function MovieList(props) {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={6}>
+          <Typography variant="h6" className={classes.title}>
+            Movie List
+          </Typography>
+          <div className={classes.demo}>
+            <List>
+                {props.texts.map(text => (
+                <ListItem>
+                  <ListItemAvatar>
+                    <Avatar>
+                      <MovieIcon />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText primary={text} />
+                  <ListItemSecondaryAction>
+                    <IconButton onClick={() => {window.alert("update")}} edge="start" aria-label="update">
+                      <UpdateIcon />
+                    </IconButton>
+                    <IconButton onClick={() => {window.alert("delete")}} edge="end" aria-label="delete">
+                      <DeleteIcon />
+                    </IconButton>
+                  </ListItemSecondaryAction>
+                </ListItem>
+                ))}
+            </List>
+          </div>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import MovieList from './components/movieList';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<MovieList texts={['movie1', 'movie2', 'movie3']}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
