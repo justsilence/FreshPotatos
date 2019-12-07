e7918612156d935433b79a891e7731ec0ca914bf
liuquan <hansxyz08@gmail.com>
2019-12-07 14:24:37 -0500

    login  update

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState }  from 'react';
 import './App.css';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
@@ -8,6 +8,7 @@ import MovieModify from './components/MovieModify';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import { AuthContext } from "./context/auth";
 
 const movieInfo = {
   name: 'Terminator 2: Judgment Day',
@@ -42,8 +43,15 @@ const movieInfo = {
 
 const moviesList = ['movie1', 'movie2', 'movie3', 'for the sake of demo, dummy movies are there']
 
-function App() {
+function App(props) {
+  const [authTokens, setAuthTokens] = useState();
+  
+  const setTokens = (data) => {
+    localStorage.setItem("tokens", JSON.stringify(data));
+    setAuthTokens(data);
+  }
   return (
+    <AuthContext.Provider value={{ authTokens, setAuthTokens: setTokens }}>
     <BrowserRouter>
       <div className="App">
         <Navbar/>
@@ -57,6 +65,7 @@ function App() {
         </Switch>
       </div>
       </BrowserRouter>
+      </AuthContext.Provider>
   );
 }
 
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,6 @@
-import React from 'react';
+
 import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
+//import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
@@ -12,156 +12,216 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 
-// generate style attributes
-const useStyles = makeStyles(theme => ({
-  // style for current page
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  // style for icon
-  icon: {
-    margin: theme.spacing(2),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  // style for email&password input form
-  form: {
-    width: '100%',
-    marginTop: theme.spacing(2),
-  },
-  grid: {
-    padding: theme.spacing(1),
-  },
-  // style for sign in button
-  submit: {
-    margin: theme.spacing(2, 0, 2),
-  },
-}));
 
-// function to check login
-const login = function(e, email, password) {
-  e.preventDefault();
+import React, { useState } from "react";
+import { Link, Redirect } from "react-router-dom";
+import axios from 'axios';
+import { Card, Logo, Form, Input, Button, Error } from "./AuthForms.js";
+import { useAuth } from "../context/auth";
+
+// // generate style attributes
+// const useStyles = makeStyles(theme => ({
+//   // style for current page
+//   paper: {
+//     marginTop: theme.spacing(8),
+//     display: 'flex',
+//     flexDirection: 'column',
+//     alignItems: 'center',
+//   },
+//   // style for icon
+//   icon: {
+//     margin: theme.spacing(2),
+//     backgroundColor: theme.palette.secondary.main,
+//   },
+//   // style for email&password input form
+//   form: {
+//     width: '100%',
+//     marginTop: theme.spacing(2),
+//   },
+//   grid: {
+//     padding: theme.spacing(1),
+//   },
+//   // style for sign in button
+//   submit: {
+//     margin: theme.spacing(2, 0, 2),
+//   },
+// }));
+
+// // function to check login
+// const login = function(e, email, password) {
+//   e.preventDefault();
+//   const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+//   if (email === undefined || password === undefined) {
+//     window.alert("Please provide valid email or password");
+//   } else if (email.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
+//     fetch(proxyUrl+'https://project3b-kam455.glitch.me/api/brand/microsoft', {
+//       method: 'GET',
+//       // body: JSON.stringify({'email': email, 'password': password}),
+//       headers: new Headers({
+//         'Content-Type': 'application/json'
+//       })
+//     }).then(res => res.json())
+//     .catch(error => console.error('Error:', error))
+//     .then(response => console.log('Success:', response));
+//   } else {
+//     window.alert('invalid email');
+//   }
+// }
+
+// // Login component
+// class Login extends React.Component{
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       email: '',
+//       password: '',
+//     };
+//     this.handleChangeEmail = this.handleChangeEmail.bind(this);
+//     this.handleChangePassword = this.handleChangePassword.bind(this);
+//   }
+//   handleChangeEmail(event) { this.setState({email: event.target.value}); }
+//   handleChangePassword(event) { this.setState({password: event.target.value}); }
+
+//   render(){
+//     return (
+//       <Container component="main" maxWidth="xs">
+//         <CssBaseline />
+//         <div className={this.props.classes.paper}>
+//           <Avatar className={this.props.classes.icon}>
+//             <LockOutlinedIcon />
+//           </Avatar>
+//           <Typography component="h1" variant="h4">
+//             Sign in
+//           </Typography>
+//           <form className={this.props.classes.form} noValidate>
+//             <TextField
+//               variant="outlined"
+//               margin="normal"
+//               required
+//               fullWidth
+//               id="email"
+//               label="Email Address"
+//               name="email"
+//               value={this.state.email}
+//               onChange={this.handleChangeEmail}
+//               InputProps={{
+//                 startAdornment: (
+//                   <InputAdornment position="start">
+//                     <Email />
+//                   </InputAdornment>
+//                 ),
+//               }}
+//               type='email'
+//               autoComplete="email"
+//               autoFocus
+//             />
+//             <TextField
+//               variant="outlined"
+//               margin="normal"
+//               required
+//               fullWidth
+//               id="password"
+//               label="Password"
+//               name="password"
+//               value={this.state.password}
+//               onChange={this.handleChangePassword}
+//               InputProps={{
+//                 startAdornment: (
+//                   <InputAdornment position="start">
+//                     <Password />
+//                   </InputAdornment>
+//                 ),
+//               }}
+//               type="password"
+//               autoComplete="current-password"
+//             />
+//             <Grid container>
+//               <Grid item xs className={this.props.classes.grid}>
+//                 <Button
+//                   type="submit"
+//                   fullWidth
+//                   variant="contained"
+//                   color="primary"
+//                   className={this.props.classes.submit}
+//                   onClick={(e) => {login(e, this.state.email, this.state.password)}}
+//                 >
+//                   Sign In
+//                 </Button>
+//               </Grid>
+//               <Grid item xs className={this.props.classes.grid}>
+//                 <Button
+//                   type="submit"
+//                   fullWidth
+//                   variant="contained"
+//                   color="primary"
+//                   className={this.props.classes.submit}
+//                   href='/detail'
+//                 >
+//                   Sign Up
+//                 </Button>
+//               </Grid>
+//             </Grid>
+//           </form>
+//         </div>
+//       </Container>
+//     );
+//   }
+// }
+
+function Login(props) {
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const { setAuthTokens } = useAuth();
+
+
+function postLogin() {
   const proxyUrl = "https://cors-anywhere.herokuapp.com/";
-  if (email === undefined || password === undefined) {
-    window.alert("Please provide valid email or password");
-  } else if (email.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
-    fetch(proxyUrl+'https://project3b-kam455.glitch.me/api/brand/microsoft', {
-      method: 'GET',
-      // body: JSON.stringify({'email': email, 'password': password}),
-      headers: new Headers({
-        'Content-Type': 'application/json'
-      })
-    }).then(res => res.json())
-    .catch(error => console.error('Error:', error))
-    .then(response => console.log('Success:', response));
-  } else {
-    window.alert('invalid email');
-  }
+  axios.get(proxyUrl+"https://project3b-kam455.glitch.me/api/brand/microsoft", {
+    userName,
+    password
+  }).then(result => {
+    if (result.status === 200) {
+      setAuthTokens(result.data);
+      setLoggedIn(true);
+    } else {
+      setIsError(true);
+    }
+  }).catch(e => {
+    setIsError(true);
+  });
 }
 
-// Login component
-class Login extends React.Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      password: '',
-    };
-    this.handleChangeEmail = this.handleChangeEmail.bind(this);
-    this.handleChangePassword = this.handleChangePassword.bind(this);
-  }
-  handleChangeEmail(event) { this.setState({email: event.target.value}); }
-  handleChangePassword(event) { this.setState({password: event.target.value}); }
-
-  render(){
-    return (
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <div className={this.props.classes.paper}>
-          <Avatar className={this.props.classes.icon}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h4">
-            Sign in
-          </Typography>
-          <form className={this.props.classes.form} noValidate>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              value={this.state.email}
-              onChange={this.handleChangeEmail}
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <Email />
-                  </InputAdornment>
-                ),
-              }}
-              type='email'
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="password"
-              label="Password"
-              name="password"
-              value={this.state.password}
-              onChange={this.handleChangePassword}
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <Password />
-                  </InputAdornment>
-                ),
-              }}
-              type="password"
-              autoComplete="current-password"
-            />
-            <Grid container>
-              <Grid item xs className={this.props.classes.grid}>
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  color="primary"
-                  className={this.props.classes.submit}
-                  onClick={(e) => {login(e, this.state.email, this.state.password)}}
-                >
-                  Sign In
-                </Button>
-              </Grid>
-              <Grid item xs className={this.props.classes.grid}>
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  color="primary"
-                  className={this.props.classes.submit}
-                  href='/detail'
-                >
-                  Sign Up
-                </Button>
-              </Grid>
-            </Grid>
-          </form>
-        </div>
-      </Container>
-    );
-  }
+if (isLoggedIn) {
+  return <Redirect to='/'/>;
 }
 
-export default function Hook() {
-  const classes = useStyles();
-  return <Login classes={classes}>Hook</Login>;
+return (
+  <Card>
+    <Form>
+      <Input
+        type="username"
+        value={userName}
+        onChange={e => {
+          setUserName(e.target.value);
+        }}
+        placeholder="email"
+      />
+      <Input
+        type="password"
+        value={password}
+        onChange={e => {
+          setPassword(e.target.value);
+        }}
+        placeholder="password"
+      />
+      <Button onClick={postLogin}>Sign In</Button>
+    </Form>
+    <Link to="/signup">Don't have an account?</Link>
+      { isError &&<Error>The username or password provided were incorrect!</Error> }
+  </Card>
+);
 }
+
+export default Login;
