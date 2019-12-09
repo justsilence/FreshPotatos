3e9ccc70f399f3ca603440a3aec5527c71985657
liuquan <hansxyz08@gmail.com>
2019-12-09 12:24:29 -0500

    chenge movielist style to gridlist

--- client/src/components/MovieList.js
+++ client/src/components/MovieList.js
@@ -11,6 +11,10 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import ArrowRight from '@material-ui/icons/ArrowRight';
 // import MovieIcon from '@material-ui/icons/Movie';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import InfoIcon from '@material-ui/icons/Info';
 
 // self defined styles
 const useStyles = makeStyles(theme => ({
@@ -23,6 +27,20 @@ const useStyles = makeStyles(theme => ({
   },
   title: {
     margin: theme.spacing(4, 0, 2),
+  },
+  gridroot: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    width: 700,
+    height: 1000,
+  },
+  icon: {
+    color: 'rgba(255, 255, 255, 0.54)',
   },
 }));
 
@@ -49,7 +67,7 @@ class MovieList extends React.Component{
       })
       .then(res => res.json())
       .then(res => res['movies'])
-      .then(movies => movies.map((movie) => {return {'name': movie.name, 'id': movie._id, 'image': movie.image}}))
+      .then(movies => movies.map((movie) => {return {'name': movie.name, 'id': movie._id, 'image': movie.image,'rating':movie.rating}}))
       .then(movies => this.setState({movies: movies}));
     }else{
       fetchURL = 'https://web-final-demo.azurewebsites.net/api/movie/search' + this.props.routerProps.location.search;
@@ -61,20 +79,41 @@ class MovieList extends React.Component{
       })
       .then(res => res.json())
       .then(res => res['result'])
-      .then(movies => movies.map((movie) => {return {'name': movie.name, 'id': movie._id, 'image': movie.image}}))
+      .then(movies => movies.map((movie) => {return {'name': movie.name, 'id': movie._id, 'image': movie.image,'rating':movie.rating}}))
       .then(movies => this.setState({movies: movies}));
     }
   }
 
   render(){
     return (
-      <div className={this.props.classes.root}>
+      <div>
+      <div className={this.props.classes.gridroot}>
+              <GridList cellHeight={300} className={this.props.classes.gridList}>
+                <GridListTile key="Header" cols={2} style={{ height: 70 }}>
+                  <h2 component="div">What we found</h2>
+                </GridListTile>
+                {this.state.movies.map(m => (
+                  <GridListTile key={m.img} >
+                     <img src={m.image} alt={m.name} />
+                     <GridListTileBar
+                      title={m.name}
+              subtitle={<span>rating: {m.rating}</span>}
+              actionIcon={
+                <IconButton aria-label={`info about ${m.name}`}  onClick ={(e) => {e.preventDefault(); window.location.href=('/detail/'+m.id)}} className={this.props.classes.icon}>
+                  <InfoIcon />
+                </IconButton>
+              }/>
+                </GridListTile>
+                ))}
+              </GridList>
+            </div>
+      {/* <div className={this.props.classes.root}>
         <Grid container spacing={2}>
           <Grid item xs={12} md={12}>
             <Typography variant="h6" className={this.props.classes.title}/>
             <div className={this.props.classes.demo}>
               <List>
-                {this.state.movies === []?(this.state.movies.map(movie => (
+                {this.state.movies.map(movie => (
                   <ListItem key={movie.id}>
                     <ListItemAvatar>
                       <Avatar alt={movie.name} src={movie.image} />
@@ -86,11 +125,12 @@ class MovieList extends React.Component{
                       </IconButton>
                     </ListItemSecondaryAction>
                   </ListItem>
-                ))):( <h1>Sorry, no results found!</h1>)}
+                }
               </List>
             </div>
           </Grid>
         </Grid>
+      </div> */}
       </div>
     );
   }
