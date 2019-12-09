ad17421140358651325267528e65b79f192bec33
liuquan <hansxyz08@gmail.com>
2019-12-09 17:58:13 -0500

    button width

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -84,6 +84,17 @@ const useStyles = makeStyles(theme => ({
     padding: theme.spacing(1, 5, 1, 5),
     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
   },
+  buttonNav: {
+    marginRight: theme.spacing(0),
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    borderRadius: theme.spacing(1),
+    border: theme.spacing(0),
+    color: 'white',
+    height: theme.spacing(5),
+    width: theme.spacing(5),
+    padding: theme.spacing(1, 5, 1, 5),
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  },
 }));
 
 export default function Navbar({ component: Component, ...rest }) {
@@ -241,12 +252,12 @@ export default function Navbar({ component: Component, ...rest }) {
           <div className={classes.sectionMobile}>
           {window.sessionStorage.getItem('is_login') ? 
           (
-            <Button className={classes.button} onClick={handleMobileMenuOpen} >
+            <Button className={classes.buttonNav} onClick={handleMobileMenuOpen} >
               <MoreIcon />
             </Button>
           ):
           (
-            <Button className={classes.button} onClick={event =>  window.location.href='/login'}>
+            <Button className={classes.buttonNav} onClick={event =>  window.location.href='/login'}>
               LogIn
             </Button>)}
           </div>
